ad23565221e1a80aab46c2ced2d3f1dec1d3b7af
toye1990 <ifetoyeobafemi@gmail.com>
2023-07-19 03:59:31 +0100

    my fourth python coding

---
+++ python-hello_world/3-last_digit.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+if number > 5:
+    print("last digit of {} is greater than 5".format(number))
+elif number == 0:
+    print("last digit of {} is 0".format(number))
+elif number < 6 != 0:
+    print("last digit of {} is less than 6 and not 0".format(number))
+    
