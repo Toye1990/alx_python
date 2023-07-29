7cae080e3aa60a6be21a5572146d623e7ce1b9fc
toye1990 <ifetoyeobafemi@gmail.com>
2023-07-29 19:29:42 +0100

    work readjusted

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -7,3 +7,6 @@ def print_matrix_integer(matrix=[[]]):
             element = "{:d}".format(matrix[i][j])
             print(element, end=", ")
         print()
+
+matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+print_matrix_integer(matrix)
