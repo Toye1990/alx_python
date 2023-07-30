efbe4205c6740bb35d69295550bdf829339d5a8b
toye1990 <ifetoyeobafemi@gmail.com>
2023-07-30 15:49:42 +0100

    printed newly

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -6,7 +6,8 @@ if __name__ == "__main__":
   except ZeroDivisionError:
    return None
   finally:
-    print("{}".format(result))
+    result = safe_print_division(a, b)
+    print("{:d} / {:d} = {}".format(a, b, result))   
     return result
 
 
