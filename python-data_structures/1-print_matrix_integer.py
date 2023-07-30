e990321957c207914b1a9e4d5e52c87334ca80c6
toye1990 <ifetoyeobafemi@gmail.com>
2023-07-30 18:31:25 +0100

    matrix worked on

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
  def print_matrix_integer(matrix=[[]]):
-    rows = len(matrix)
-    columns = len(matrix[0]) if rows > 0 else 0
-
-    for i in range(rows):
-        for j in range(columns):
-            element = "{:d}".format(matrix[i][j])
-            print(element, end=", ")
-        print()
+   for row in matrix:
+     for i in range(len(row)):
+       if i == len(row) - 1:
+         print(row[i])
+       else: 
+         print(row[i], end=", ")
+       
 
