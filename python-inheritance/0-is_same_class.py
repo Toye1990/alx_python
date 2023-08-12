9c51bedb9d1b0ce7af35cfd7bd0e798e920895b5
toye1990 <ifetoyeobafemi@gmail.com>
2023-08-12 18:11:31 +0100

    instance function corrected

--- python-inheritance/0-is_same_class.py
+++ python-inheritance/0-is_same_class.py
@@ -3,7 +3,9 @@
 module to used later
 """
 
-#used function to is_same_class() to confirm the instance of a class
+"""
+used function to is_same_class() to confirm the instance of a class
+"""
 def is_same_class(obj, a_class):
     #if the object is exactly an instance of the specified class
     if isinstance(obj, a_class):
