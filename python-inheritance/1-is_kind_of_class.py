a48484305e8436b2424274b80ee9ad9dfa13c408
toye1990 <ifetoyeobafemi@gmail.com>
2023-08-12 18:22:26 +0100

    second instance fixed

--- python-inheritance/1-is_kind_of_class.py
+++ python-inheritance/1-is_kind_of_class.py
@@ -1,5 +1,14 @@
+"""
+module will be used later
+"""
 def is_kind_of_class(obj, a_class):
+    """
+    use function is_same_class() to confirm the instance of a class
+    """
     if isinstance(obj, a_class):
+        """
+        use function isinstance function to confirm the instance of a class
+        """
         return True
     if isinstance(obj, type):
         return issubclass(obj, a_class)
