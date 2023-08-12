0d9e159e2a3c493f59a6425db9f900079dd3f16b
toye1990 <ifetoyeobafemi@gmail.com>
2023-08-12 18:54:26 +0100

    fifth instance work done

--- python-inheritance/5-base_geometry.py
+++ python-inheritance/5-base_geometry.py
@@ -1,9 +1,18 @@
-class BaseGeometry:
-    def area(self):
-        raise Exception("area() is not implemented")
-    def integer_validator(self, name, value):
 
-        if not isinstance(value, int):
-            TypeError(f"<name> must be an integer")
-        if self.value <= 0:
-            ValueError(f"<name> must be greater than")
+"""
+module will be used later
+"""
+class BaseGeometry:
+ """
+ BaseGeometry class to be used to raised an exception
+"""
+ def area(self):
+    """
+    BaseGeometry class to be used to raised an exception
+    """
+    raise Exception("area() is not implemented")
+ def integer_validator(self, name, value):
+    if not isinstance(value, int):
+       TypeError(f"{name} must be an integer")
+    if value <= 0:
+        ValueError(f"{name} must be greater than")
