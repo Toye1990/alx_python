7dad140cd3c766ca6af99d969264cb8c4f4450a2
toye1990 <ifetoyeobafemi@gmail.com>
2023-08-12 18:33:47 +0100

    base class done

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -1,5 +1,10 @@
-#write an empty class
+"""
+module will be used later
+"""
 class BaseGeometry:
+ """
+ BaseGeometry class is outlined to determined details of functions and attributes used in the class
+ """
  def __dir__(cls):
   return [attribute for attribute in super().__dir__() if attribute != "__init_subclass__"]
  
