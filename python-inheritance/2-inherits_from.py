32b55987486cd7a0db4a6f3eb219837551c9483c
toye1990 <ifetoyeobafemi@gmail.com>
2023-08-12 18:28:00 +0100

    third instance fixed

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -1,7 +1,19 @@
+"""
+module will be used later
+"""
 def inherits_from(obj, a_class):
+   """
+    use function inherits_from() to confirm the instance of a class
+    """
    if isinstance(obj, a_class):
+      """
+      use function isinstance() to confirm the instance of a class
+      """
       return True
    if not isinstance(obj, type):
+      """
+      use function isinstance() with type as class argument to check if object is an instance of a class
+      """
       return False
    while True:
       if issubclass(obj, type):
