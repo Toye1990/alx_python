9a69c9a05d3aa13fbd83b2b402160192a8275df1
toye1990 <ifetoyeobafemi@gmail.com>
2023-08-12 18:14:46 +0100

    instance deed done

--- python-inheritance/0-is_same_class.py
+++ python-inheritance/0-is_same_class.py
@@ -2,12 +2,10 @@
 """
 module to used later
 """
-
-"""
-used function to is_same_class() to confirm the instance of a class
-"""
 def is_same_class(obj, a_class):
-    #if the object is exactly an instance of the specified class
+    """
+    used function to is_same_class() to confirm the instance of a class
+    """
     if isinstance(obj, a_class):
         return True
     #otherwise False
