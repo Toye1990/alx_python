2832ea7c5bead4b17a139090a872d95c40ac800a
toye1990 <ifetoyeobafemi@gmail.com>
2023-08-12 20:02:53 +0100

    rectangle work done

--- python-inheritance/7-rectangle.py
+++ python-inheritance/7-rectangle.py
@@ -1,4 +1,10 @@
+"""
+module will be used later
+""" 
 class BaseGeometry:
+    """
+    class BaseGeometry to reused
+    """
     def area(self):
         raise Exception("area() is not implemented")
     def integer_validator(self, name, value):
@@ -8,13 +14,16 @@ class BaseGeometry:
         if self.value <= 0:
             ValueError(f"<name> must be greater than")
 class Rectangle(BaseGeometry):
+    """
+    class BaseGeometry reused
+    """
     def __init__(self, width, height):
         self.width = self.integer_validator("width", width)
         self.height = self.integer_validator("height", height)
     def area(self):
         return self.width * self.height
     def __str__(self):
-        return f"[rectangle] {self.width} {self.height}"
+        return ("[rectangle] {}/{}".format(self.width, self.height))
     def __repr__(self):
         return self.__str__() 
 
