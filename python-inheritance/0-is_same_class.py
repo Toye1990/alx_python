da27a3e8ca63c853695adae996f92241aa82d0b6
toye1990 <ifetoyeobafemi@gmail.com>
2023-08-12 18:03:28 +0100

    instance correct

--- python-inheritance/0-is_same_class.py
+++ python-inheritance/0-is_same_class.py
@@ -1,2 +1,31 @@
+#!/usr/bin/python3
+"""
+module to used later
+"""
 def is_same_class(obj, a_class):
-    return type(obj) == a_class
+    #if the object is exactly an instance of the specified class
+    if isinstance(obj, a_class):
+        return True
+    #otherwise False
+    else:
+        return False
+
+
+
+
+
+'''class Phone:
+    color = ""
+    width = ""
+    height = ""
+    def __init__(self, mycolor, mywidth, myheight):
+        self.width = mywidth
+        self.height = myheight
+        self.color = mycolor
+    def makephone(self):
+        return print("You can make calls")
+callnow = Phone('red', 50, 40)
+print(callnow.color)
+print(callnow.width)
+print(callnow.makephone())'''
+        
--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -1,2 +1,9 @@
+#write an empty class
 class BaseGeometry:
-    pass
+ def __dir__(cls):
+  return [attribute for attribute in super().__dir__() if attribute != "__init_subclass__"]
+ 
+base = BaseGeometry()
+print(dir(base))
+
+
