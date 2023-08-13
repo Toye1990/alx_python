76c5b7415e4b92e5d2681e5a506e17a0f3e0aa66
toye1990 <ifetoyeobafemi@gmail.com>
2023-08-13 17:53:39 +0100

    instance 2

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -15,14 +15,8 @@ class Rectangle(Base):
  """
  class rectangle to used to manage id in this project and future project
  """ 
- width = None
- height = None
-
+ 
  def __init__(self, width, height, x=0, y=0, id=None):
-   
-  """
- class constructor to initiate the rectangle
- """
   super().__init__(id)
   self.width = width
   self.height = height
