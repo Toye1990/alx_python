70c4432ae5b9670a07e7c8a07edc77f68956c995
toye1990 <ifetoyeobafemi@gmail.com>
2023-08-13 17:42:43 +0100

    rec now long

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,7 +1,9 @@
 from models.base import Base
 """
 Module Base import for inheritance purpose, this will validate the instance in
-rectangle class
+rectangle class. Lorem ipsum dolor sit amet consectetur adipiscing elit tempus natoque faucibus viverra, litora odio vitae cras blandit ut est integer nisl in accumsan himenaeos, facilisis eu maecenas lectus ante platea commodo curabitur fermentum et. 
+Pellentesque euismod proin himenaeos nullam felis eros maecenas egestas tristique, pretium imperdiet morbi habitasse ullamcorper cras ad accumsan turpis, quis congue phasellus ultricies potenti volutpat varius torquent. 
+Curae ante congue eu parturient imperdiet dui tempus penatibus, ultrices 
 """ 
 class Rectangle(Base):
  """
