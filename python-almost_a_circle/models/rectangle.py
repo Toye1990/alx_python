c2daebbc0b22d80cec410a23560ffee484262455
toye1990 <ifetoyeobafemi@gmail.com>
2023-08-13 17:08:19 +0100

    rec corrected

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -62,19 +62,3 @@ def y(self, y):
   """Set the y value of the rectangle."""
   self.__y = y
 
-b1 = Base()
-print(b1.id)
-
-b2 = Base()
-print(b2.id)
-
-b3 = Base()
-print(b3.id)
-
-b4 = Base(12)
-print(b4.id)
-
-b5 = Base()
-print(b5.id)
-
- 
