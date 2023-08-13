958b46fda49154ac02f713284d74cc5440ce1794
toye1990 <ifetoyeobafemi@gmail.com>
2023-08-13 17:17:52 +0100

    work done

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,21 +1,7 @@
+from base import Base
 """
-module will be used later
+module base import as inheritance for rectangle clase
 """ 
-class Base:
- """
-class base to used to manage id in this project and future project
-""" 
- __nb_objects = 0
- def __init__(self, id=None):
-  """
-  A constructor to instantiate the instant value
-  """ 
-  if id is not None:
-   self.id = id
-  else:
-   self.id = Base.__nb_objects + 1
-   Base.__nb_objects += 1
-   
 class Rectangle(Base):
  __width = None
  __heigh = None
