3b365a44279c070d95d9111ba56b073f85fb6f23
toye1990 <ifetoyeobafemi@gmail.com>
2023-08-13 18:09:45 +0100

    revise instance

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -12,55 +12,60 @@ Pellentesque euismod proin himenaeos nullam felis eros maecenas egestas tristiqu
 Curae ante congue eu parturient imperdiet dui tempus penatibus, ultrices 
 """ 
 class Rectangle(Base):
- """
- class rectangle to used to manage id in this project and future project
- """ 
- 
- def __init__(self, width, height, x=0, y=0, id=None):
-  super().__init__(id)
-  self.width = width
-  self.height = height
-  self.x = x
-  self.y = y
-  self.id = id
-  
-  @property
-  def width(self):
-   """Get the width of the rectangle."""
-   return self.__width
-  
-  @width.setter
-  def width(self, width):
-   """Set the width of the rectangle."""
-   self.__width = width
+    """class rectangle to used to manage id in this project and future project"""
 
-@property
-def height(self):
- """Get the height of the rectangle."""
- return self.__height
+    # Private instance attributes
+    __width = None
+    __height = None
+    __x = None
+    __y = None
 
-@height.setter
-def height(self, height):
-  """Set the height of the rectangle."""
-  self.__height = height
+    # Public getters and setters
 
-@property
-def x(self):
- """Get the value x of the rectangle."""
- return self.__x
+    @property
+    def width(self):
+        """Get the width of the rectangle."""
+        return self.__width
 
-@x.setter
-def x(self, x):
-  """Set the x value of the rectangle."""
-  self.__x = x
+    @width.setter
+    def width(self, width):
+        """Set the width of the rectangle."""
+        self.__width = width
 
-@property
-def y(self):
- """Get the y value of the rectangle."""
- return self.__height
+    @property
+    def height(self):
+        """Get the height of the rectangle."""
+        return self.__height
 
-@y.setter
-def y(self, y):
-  """Set the y value of the rectangle."""
-  self.__y = y
+    @height.setter
+    def height(self, height):
+        """Set the height of the rectangle."""
+        self.__height = height
 
+    @property
+    def x(self):
+        """Get the x-coordinate of the rectangle."""
+        return self.__x
+
+    @x.setter
+    def x(self, x):
+        """Set the x-coordinate of the rectangle."""
+        self.__x = x
+
+    @property
+    def y(self):
+        """Get the y-coordinate of the rectangle."""
+        return self.__y
+
+    @y.setter
+    def y(self, y):
+        """Set the y-coordinate of the rectangle."""
+        self.__y = y
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Initialize a new rectangle."""
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
