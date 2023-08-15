5abe1f9c1d60ea8add725ff5bc2d1f8ff8c403e5
toye1990 <ifetoyeobafemi@gmail.com>
2023-08-15 21:11:42 +0100

    try response

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -2,9 +2,4 @@
 import requests
 """The request module imported to be used to fetch data"""
 
-def main():
- response = requests.get('https://alu-intranet.hbtn.io/status')
- if response.status == 200:
-    print(response.content)
- else:
-    print('error fetching status: {}'.format(response.status_code))
+response = requests.get('https://alu-intranet.hbtn.io/status')
