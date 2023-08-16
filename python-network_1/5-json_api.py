742c76a1a3dbe8f5b8f01038757d7c7cdcf18dae
toye1990 <ifetoyeobafemi@gmail.com>
2023-08-16 18:57:32 +0100

    touch

---
+++ python-network_1/5-json_api.py
@@ -0,0 +1,9 @@
+"""The request module imported to be used to fetch data"""
+import requests
+"""The request module imported to be used to fetch data"""
+import sys
+"""The request module imported to be used to fetch data"""
+def letter_case(q):
+  url = 'http://0.0.0.0:5000/search_user'
+  response = response.POST(url, data=data)
+  return response.JSON()
