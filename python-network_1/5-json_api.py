9d33cf9385906f4bc3893dd7e09f3fc34138f177
toye1990 <ifetoyeobafemi@gmail.com>
2023-08-16 19:04:30 +0100

    try

--- python-network_1/5-json_api.py
+++ python-network_1/5-json_api.py
@@ -6,4 +6,15 @@ import sys
 def letter_case(q):
   url = 'http://0.0.0.0:5000/search_user'
   response = response.POST(url, data=data)
-  return response.JSON()
+  return response.JSON(q)
+
+if __name__ == '__main__':
+  q = sys.argv[1] if len(sys.argv) > 1 else ''
+  response = letter_case(q)
+
+  if response is None:
+    print('Not a valid JSON')
+  elif not response:
+    print('No result')
+  else:
+    print('[{}] {}'.format(response['id'], response['name']))
