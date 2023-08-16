0088819034c2f6c547916313aefd2edf17b405f5
toye1990 <ifetoyeobafemi@gmail.com>
2023-08-16 18:11:38 +0100

    take it

---
+++ python-network_1/1-hbtn_header.py
@@ -0,0 +1,11 @@
+"""The request module imported to be used to fetch data"""
+import requests
+"""The request module imported to be used to fetch data"""
+import sys
+"""The request module imported to be used to fetch data"""
+
+def get_request(URL):
+    """The request module imported to be used to fetch data"""
+    response = response.get(URL)
+    return response.header['x-header-Id']
+
