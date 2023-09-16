3e897f144e4fd568b9d03cfe3078012c7052412c
toye1990 <ifetoyeobafemi@gmail.com>
2023-09-16 13:53:48 +0100

    TRY IT NOW

--- python-web_framework/3-python_route.py
+++ python-web_framework/3-python_route.py
@@ -1,19 +1,13 @@
-"""flask is imported to use as a server run the database. Lorem ipsum dolor sit amet consectetur adipiscing elit tempus natoque faucibus viverra, litora
- odio vitae cras blandit ut est integer nisl in accumsan himenaeos
- Lorem ipsum dolor sit amet consectetur adipiscing elit tempus natoque faucibus viverra, litora odio vitae cras blandit ut est integer nisl in accumsan himenaeos, facilisis eu maecenas lectus ante platea commodo curabitur fermentum et. 
-Pellentesque euismod proin himenaeos nullam felis eros maecenas egestas tristique, pretium imperdiet morbi habitasse ullamcorper cras ad accumsan turpis, quis congue phasellus ultricies potenti volutpat varius torquent. 
-Curae ante congue eu parturient imperdiet dui tempus penatibus, ultrices magnis massa dapibus sed lacus aenean porta aliquam, pretium consequat diam leo velit phasellus quis.
-
-Dignissim velit molestie laoreet gravida egestas quis ante cursus sapien tincidunt hac, dui suspendisse pharetra netus fringilla pulvinar penatibus vivamus orci bibendum. Integer sociosqu nostra taciti ultricies ad pulvinar per, rhoncus sociis nascetur interdum tempor auctor sed dapibus, vivamus proin senectus ligula ante erat. Curae aptent ut erat hendrerit mi hac dignissim, pulvinar id semper inceptos quam felis sem vel, luctus auctor metus dui lobortis dis.
-,
-"""
+#flask is imported to use as a server run the database. Lorem ipsum dolor sit amet consectetur adipiscing elit tempus natoque faucibus viverra, litora
+#odio vitae cras blandit ut est integer nisl in accumsan himenaeos,
 from flask import Flask
 
-"""Create app variable intialize flask network"""
+#Create app variable intialize flask network#
 app=Flask(__name__)
 
 """Create app variable intialize flask network which will initiate the return of 
 HELLO HBNB"""
+# Define routes with the strict_slashes=False option
 @app.route("/", strict_slashes=False)
 def hello():
  return "Hello HBNB!"
